a8bb9d0721a1902748843616417c7d71d607542c
GalaxianUser <GalaxianUser@users.noreply.github.com>
2016-09-04 11:17:22 +0200

    Create pokedex.js

---
+++ mods/Summer-Cup-2016/pokedex.js
@@ -0,0 +1,16 @@
+'use strict';
+
+exports.BattlePokedex = {
+    weedle: {
+		inherit: true,
+		baseStats: {hp:100, atk:250, def:80, spa:10, spd:125, spe:60},
+    },
+       ditto: {
+		inherit: true,
+		baseStats: {hp:80, atk:1, def:250, spa:250, spd:250, spe:60},
+    },
+       excadrill: {
+		inherit: true,
+		baseStats: {hp:100, atk:150, def:60, spa:50, spd:65, spe:40},
+    },
+};
